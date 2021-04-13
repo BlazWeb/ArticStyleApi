f30a105927b1aca1e1beaa04f246f2cde2e7d96a
José Manuel Martínez Camacho <54948617+Benz-Solutions@users.noreply.github.com>
2021-04-13 21:44:27 +0200

    tf

--- models/user.go
+++ models/user.go
@@ -1,16 +1,41 @@
 package models
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+type UserJSON map[string]interface{}
+
+func (u *UserJSON) Scan(val interface{}) error {
+	switch v := val.(type) {
+	case []byte:
+		json.Unmarshal(v, &u)
+		return nil
+	case string:
+		json.Unmarshal([]byte(v), &u)
+		return nil
+	default:
+		return errors.New(fmt.Sprintf("Unsupported type: %T", v))
+	}
+}
+func (u *UserJSON) Value() (driver.Value, error) {
+	return json.Marshal(u)
+}
+
 type User struct {
-	Id             int64  `json:"id,omitempty"`
-	Username       string `json:"username,omitempty"`
-	Name           string `json:"name,omitempty"`
-	LastName       string `json:"last_name,omitempty"`
-	DateRegistered string `json:"date_registered,omitempty"`
-	Birthday       string `json:"birthday,omitempty"`
-	Password       string `json:"password,omitempty"`
-	Email          string `json:"email,omitempty"`
-	Rank           int    `json:"rank,omitempty"`
-	Img            string `json:"img,omitempty"`
+	Id             int64    `json:"id,omitempty"`
+	Username       string   `json:"username,omitempty"`
+	Name           string   `json:"name,omitempty"`
+	LastName       string   `json:"last_name,omitempty"`
+	DateRegistered string   `json:"date_registered,omitempty"`
+	Birthday       string   `json:"birthday,omitempty"`
+	Password       string   `json:"password,omitempty"`
+	Email          string   `json:"email,omitempty"`
+	Rank           int      `json:"rank,omitempty"`
+	Img            UserJSON `json:"img,omitempty"`
 }
 
 /*RespuestaLogin tiene el token que se devuelve con el login */
